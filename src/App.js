29e9e0704914f75e07923acb6190b9b86c215970
dattavenkataramana <bavirisettidvramana@gmail.com>
2024-12-09 19:30:32 +0530

    commi message 1234

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Employees from './Employees';
-// import JobOrder from './JobOrder';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Employees />} />
-//         <Route path="/joborder" element={<JobOrder />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
