00420b0c123a463bd391fece0c54b829eacdc625
dattavenkataramana <bavirisettidvramana@gmail.com>
2024-12-09 19:21:23 +0530

    commi message 1234

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Employees from './Employees';
+// import JobOrder from './JobOrder';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Employees />} />
+//         <Route path="/joborder" element={<JobOrder />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Employees from './Employees';
 import JobOrder from './JobOrder';
+import './App.css';
 
 const App = () => {
   return (
     <Router>
+      <div className="navbar">
+        <Link to="/" className="nav-link">Employees</Link>
+        <Link to="/joborder" className="nav-link">Job Order Summary</Link>
+      </div>
       <Routes>
         <Route path="/" element={<Employees />} />
         <Route path="/joborder" element={<JobOrder />} />
--- src/Employees.js
+++ src/Employees.js
@@ -139,6 +139,7 @@ const Employees = () => {
           </div>
         </div>
       )}
+      
     </div>
   );
 };
