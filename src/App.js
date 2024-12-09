56a054c8b8b53ebbabba8b327acbca3c04b3219d
dattavenkataramana <bavirisettidvramana@gmail.com>
2024-12-09 16:03:49 +0530

    43 message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Employees from './Employees';
 import JobOrder from './JobOrder';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Employees />} />
         <Route path="/joborder" element={<JobOrder />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
